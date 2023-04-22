9a3102432ccee3d297b2f6200191a86e04d143c0
DelroyCampbellR <delroycr@hotmail.com>
2023-04-21 21:00:31 -0600

    Update

--- Minor practices/5/Program.cs
+++ Minor practices/5/Program.cs
@@ -10,73 +10,55 @@ namespace Tarea7
             double saldoAr = 0;
             int opcion;
 
-            Console.WriteLine("[Banco Promérica]");
+            Console.WriteLine("| Banco Promérica |");
             Console.WriteLine();
             Console.WriteLine("Digite su nombre: ");
-            nombreAr = Console.ReadLine();
+            nombreAr = Console.ReadLine() ?? "";
             Console.WriteLine("Digite sus apellidos: ");
-            apellidosAr = Console.ReadLine();
+            apellidosAr = Console.ReadLine() ?? "";
             Console.WriteLine("Digite su dirección de domicilio: ");
-            direccionAr = Console.ReadLine();
+            direccionAr = Console.ReadLine() ?? "";
             Console.WriteLine("Digite su código de RFC: ");
-            rfcAr = Console.ReadLine();
+            rfcAr = Console.ReadLine() ?? "";
 
-            CuentaBancaria cliente = new CuentaBancaria(nombreAr, apellidosAr, saldoAr, direccionAr, rfcAr);
+            CuentaBancaria cliente = new CuentaBancaria(nombreAr, apellidosAr, direccionAr, rfcAr, saldoAr);
 
-            Console.WriteLine("Elija la opción que desea: ");
-            Console.WriteLine("1. Depósito");
-            Console.WriteLine("2. Retiro");
-            Console.WriteLine("3. Consultar Saldo");
-            Console.WriteLine("4. Mostrar información de la cuenta");
-            Console.WriteLine("5. Salir");
-            opcion = Convert.ToInt32(Console.ReadLine());
+            opcion = cliente.Opciones();
 
             do
             {
-                    switch (opcion)
-                    {
-                        case 1:
-                            double montoAr;
-                            Console.WriteLine("Digite el monto a depositar: ");
-                            montoAr = Convert.ToDouble(Console.ReadLine());
-                            Console.WriteLine("Depósito realizado con éxito. Su nuevo saldo es: {0}", cliente.Deposito(montoAr));
-
-                        cliente.Opciones();
-                        opcion = Convert.ToInt32(Console.ReadLine());
+                switch (opcion)
+                {
+                    case 1:
+                        double montoAr;
+                        Console.WriteLine("Digite el monto a depositar");
+                        montoAr = Convert.ToDouble(Console.ReadLine());
+                        Console.WriteLine($"Depósito realizado con éxito. Su nuevo saldo es {cliente.Deposito(montoAr)}");
                         break;
 
-                        case 2:
-                            double retiroAr;
-                            Console.WriteLine("Digite el monto a retirar: ");
-                            retiroAr = Convert.ToDouble(Console.ReadLine());
-                            cliente.Retiro(retiroAr);
-
-                        cliente.Opciones();
-                        opcion = Convert.ToInt32(Console.ReadLine());
+                    case 2:
+                        double retiroAr;
+                        Console.WriteLine("Digite el monto a retirar: ");
+                        retiroAr = Convert.ToDouble(Console.ReadLine());
+                        cliente.Retiro(retiroAr);
                         break;
 
-                        case 3:
-                            cliente.ConsultaSaldo();
-
-                        cliente.Opciones();
-                        opcion = Convert.ToInt32(Console.ReadLine());
+                    case 3:
+                        cliente.ConsultaSaldo();
                         break;
 
-                        case 4:
-                            Console.WriteLine();
-                            Console.WriteLine(cliente.ToString());
-
-                        cliente.Opciones();
-                        opcion = Convert.ToInt32(Console.ReadLine());
+                    case 4:
+                        Console.WriteLine();
+                        Console.WriteLine(cliente.ToString());
                         break;
 
-                        default:
-                            Console.WriteLine("Número incorrecto. Intente de nuevo ingresando únicamente números de 1 a 5.");
-
-                        cliente.Opciones();
-                        opcion = Convert.ToInt32(Console.ReadLine());
+                    default:
+                        Console.WriteLine("Número incorrecto. Intente de nuevo ingresando únicamente un número entre 1 a 5.");
                         break;
-                    }; 
+                }
+
+                opcion = cliente.Opciones();
+
             } while (opcion != 5);
         }
     }
@@ -84,13 +66,13 @@ namespace Tarea7
     {
         string nombre, apellidos, direccion, rfc;
         double saldo;
-        public CuentaBancaria(string nombrePa, string apellidosPa, double saldoPa, string direccionPa, string rfcPa)
+        public CuentaBancaria(string nombrePa, string apellidosPa, string direccionPa, string rfcPa, double saldoPa)
         {
-            nombre= nombrePa;
-            apellidos= apellidosPa;
-            direccion= direccionPa;
-            rfc = rfcPa;
-            saldo = saldoPa;
+            this.nombre = nombrePa;
+            this.apellidos = apellidosPa;
+            this.direccion = direccionPa;
+            this.rfc = rfcPa;
+            this.saldo = saldoPa;
         }
         public double Deposito(double montoPa)
         {
@@ -105,12 +87,12 @@ namespace Tarea7
             if ((retiro <= saldo) && (saldo > 0))
             {
                 saldo -= retiro;
-                Console.WriteLine("Transacción realizada. Su saldo actual es: {0}", saldo);
+                Console.WriteLine($"Transacción realizada exitosamente. Su saldo actual es: {saldo}");
             }
             else
             {
                 Console.WriteLine("Saldo insuficiente");
-                Console.WriteLine("Transacción no realizada. Su saldo actual es: {0}", saldo);
+                Console.WriteLine($"Transacción no realizada. Su saldo actual es: {saldo}");
             }
             return saldo;
         }
@@ -124,15 +106,19 @@ namespace Tarea7
             mensaje = "Nombre: " + nombre + " " + "\nApellidos: " + apellidos + " " + "\nDirección: " + direccion + " " + "\nRegistro Federal de Contribuyentes: " + rfc + " " + "\nSaldo actual: " + saldo;
             return mensaje;
         }
-        public void Opciones()
+        public int Opciones()
         {
+            int opcionElegida;
             Console.WriteLine();
             Console.WriteLine("Elija la opción que desea: ");
-            Console.WriteLine("1. Depósito");
-            Console.WriteLine("2. Retiro");
-            Console.WriteLine("3. Consultar Saldo");
-            Console.WriteLine("4. Mostrar información de la cuenta");
-            Console.WriteLine("5. Salir");
+            Console.WriteLine("1. Depósito.");
+            Console.WriteLine("2. Retiro.");
+            Console.WriteLine("3. Consultar saldo.");
+            Console.WriteLine("4. Mostrar información de la cuenta.");
+            Console.WriteLine("5. Salir.");
+            opcionElegida = Convert.ToInt32(Console.ReadLine());
+
+            return opcionElegida;
         }
     }
 }
